13fabd1e41c1b14ee932944b145a7c2e279128a0
Margarita <margarita_anis@bk.ru>
2022-05-16 21:24:37 +0300

    Add project Task_10

---
+++ Task_11/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+int number = new Random().Next(100, 1000); 
+Console.WriteLine($"Number = {number}"); 
+int firstDigit = number / 100; 
+int therdDigit = number % 10; 
+Console.Write($"New Number = {firstDigit}{therdDigit}");
