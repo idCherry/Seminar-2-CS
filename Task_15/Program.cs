06d04a6852c7e18cf98e915060950cb28089b765
Margarita <margarita_anis@bk.ru>
2022-05-16 22:34:20 +0300

    Add HOMEWORK project Task_15

---
+++ Task_15/Program.cs
@@ -0,0 +1,17 @@
+﻿// программа принимает на вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+
+Console.Write("Введите число, соответствующее дню недели -> ");
+int num = int.Parse(Console.ReadLine());
+
+while (num < 8) 
+{
+    if (num == 6 || num == 7) Console.WriteLine("Выходной день");
+    else Console.WriteLine("Не выходной день");
+    break;
+}
+while (num > 7) 
+{
+    Console.WriteLine("Нет такого дня недели"); 
+    break;
+}
