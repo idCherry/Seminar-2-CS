858d7560bff88666a99e7cdd6ecb98be5ab45cfe
Margarita <margarita_anis@bk.ru>
2022-05-16 21:28:48 +0300

    Add project Task_12

---
+++ Task_12/Program.cs
@@ -0,0 +1,9 @@
+﻿// На вход - два числа, выводит кратно ли первое число второму, если нет, то вывести остаток от деления
+
+Console.Write("Первое число - > ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Второе число - > ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 % num2 == 0) Console.WriteLine("Первое число кратно второму");
+else Console.WriteLine($"Первое число не кратно второму, остаток от деления -> {num1 % num2}");
+
